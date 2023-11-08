3de37e1f6bd8939be1a6216f009df13eaa60f03f
denicemagbanua <magbanuadenice@gmail.com>
2023-11-08 15:14:26 +0800

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,40 @@
+#list data type
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+#list
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+#tuple data type
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+#tuple
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+
+#dictionary data type
+myFavoriteFruitDictionary = {
+    "Akua" : "apple",
+    "Saanvi" : "banana",
+    "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+#dictionary by name
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
